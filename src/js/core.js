159610f9d33ba17e769b520de5c4d53f2be2374e
deinichenko <deinichenko@playform.com>
2018-12-08 17:32:40 +0300

    vk api included

--- src/js/core.js
+++ src/js/core.js
@@ -19,6 +19,7 @@ export default class Core {
             VK.api('friends.get', {
                 user_id: this.params.viewer_id
             }, (response) => {
+                debugger;
                 VK.api('users.get', {
                     user_ids: response.items.join(',')
                 }, (response) => {
