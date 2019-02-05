4a18d451709125daa40e699f33b8a99a96a76d1e
Ivan Deinichenko <deathklaat89@gmail.com>
2019-02-05 21:18:29 +0300

    landing files

---
+++ src/views/Terms.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="terms">
+    <h1>This is an terms page</h1>
+    <button @click="goBack();">TEST</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "terms",
+  methods: {
+    goBack() {
+        this.$router.push('/');
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+@import '../scss/docs.scss';
+</style>
